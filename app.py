950c110dd54726e80b848f9e9faeec91ed273494
Aayush kr. Singh <111671884+aayushsing1@users.noreply.github.com>
2023-03-22 00:02:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/op')
 def index():
-    bmi = ''
-    if request.method == 'POST' and 'weight' in request.form:
-        weight = float(request.form.get('weight'))
-        height = float(request.form.get('height'))
-        bmi = calc_bmi(weight, height)
-    return render_template("index.html",bmi=bmi)
+    return render_template('index.html')
 
-def calc_bmi(weight, height):
-    return round((weight / height**2))
-   
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    weight = float(request.form['weight'])
+    height = float(request.form['height'])
+    bmi = weight / (height ** 2)
+    return render_template('result.html', bmi=bmi)
 
 if __name__ == '__main__':
     app.run(debug=True)
